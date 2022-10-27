73d01c7343a128b9279072e579dfb19be5d668f4
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-10-27 11:15:19 -0300

    importing areas done

---
+++ areas.py
@@ -0,0 +1,14 @@
+#!/bin/env python3 
+
+import math
+
+
+def triangle(base, height):
+    return base*height/2
+
+def rectangle(base, height):
+    return base*height
+
+def circle(radius):
+    return math.pi*(radius**2)
+
---
+++ importareas.py
@@ -0,0 +1,5 @@
+import areas
+
+print(areas.triangle(3,3))
+print(areas.rectangle(3,3))
+print(areas.circle(3))
